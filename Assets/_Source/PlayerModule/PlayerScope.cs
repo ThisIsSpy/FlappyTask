b24550f530f0855cc3b87d477b6d756f50d9a7cd
ThisIsSpy <art.klyachkin@gmail.com>
2025-09-11 17:21:18 +0300

    feat: add build, start VContainer integration

---
+++ Assets/_Source/PlayerModule/PlayerScope.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace PlayerModule 
+{
+    public class PlayerScope : LifetimeScope
+    {
+        [SerializeField] private PlayerStats playerStats;
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterInstance(playerStats);
+            builder.Register<PlayerModel>(Lifetime.Singleton);
+            builder.RegisterComponentInHierarchy<PlayerView>();
+            builder.RegisterComponentInHierarchy<PlayerController>();
+        }
+    }
+}
---
+++ Assets/_Source/PlayerModule/PlayerStats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerModule 
+{
+    [CreateAssetMenu(fileName = "PlayerStatsSO", menuName = "Scriptable Objects/Create new Player Stats SO")]
+    public class PlayerStats : ScriptableObject
+    {
+        [field: SerializeField] public float Speed { get; private set; }
+        [field: SerializeField] public float JumpVelocity { get; private set; }
+    }
+}
